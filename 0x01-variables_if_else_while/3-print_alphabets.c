66a8928a1be83a0534ca609ab256a9e0f4450a58
Trevour <107504887+Jon33s-lab@users.noreply.github.com>
2022-07-01 20:52:50 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,28 @@
-k
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+	l = 'A';
+	while (l <= 'Z')
+	{
+		putchar(l);
+		l++;
+
+	}
+	putchar('\n');
+	return (0);
+}
